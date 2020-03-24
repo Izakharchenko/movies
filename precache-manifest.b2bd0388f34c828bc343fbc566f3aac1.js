98264919422f3c6a31e2594c020e5915b85d5519
Ivan Zakharchenko <ipzakharchenko@gmail.com>
2020-03-24 16:46:12 +0200

    Updates

---
+++ precache-manifest.b2bd0388f34c828bc343fbc566f3aac1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6490c3cbbe0bbfcc7cf",
+    "url": "/movies/static/js/main.0c3b7e36.chunk.js"
+  },
+  {
+    "revision": "04129ba559ae92aa086e",
+    "url": "/movies/static/js/runtime~main.712e9881.js"
+  },
+  {
+    "revision": "c245b86272725ca662a1",
+    "url": "/movies/static/css/2.5f4ef4d6.chunk.css"
+  },
+  {
+    "revision": "c245b86272725ca662a1",
+    "url": "/movies/static/js/2.733faceb.chunk.js"
+  },
+  {
+    "revision": "492b45d5fbb542c93a697c561be699a9",
+    "url": "/movies/index.html"
+  }
+];
